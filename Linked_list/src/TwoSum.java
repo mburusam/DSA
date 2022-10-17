a6c83accd5a123ee6b9aefe8ba256589d5bef3a1
Samuel Mburu <samuel.mburu@tracom.co.ke>
2022-10-17 17:22:28 +0300

    Two sum leetcode:

---
+++ Linked_list/src/TwoSum.java
@@ -0,0 +1,79 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+
+   /* Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+    You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+    You can return the answer in any order.
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
+
+
+*/
+    //solution one
+   // Time complexity: O(n^2)
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i<nums.length; i++){
+            for (int j=i+1;j<nums.length;j++){
+                if(nums[i] +nums[j] == target){
+                    return new int[] {i,j};
+                }
+            }
+        }
+        return new int[] {};
+    }
+
+    //solution two
+    // Time complexity: O(n)
+    private static int[] findTwoSum(int[] nums, int target) {
+        Map<Integer, Integer> numMap = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (numMap.containsKey(complement)) {
+                return new int[] { numMap.get(complement), i };
+            } else {
+                numMap.put(nums[i], i);
+            }
+        }
+        return new int[] {};
+    }
+
+    //solution three
+    // Time complexity: O(n*log(n))
+    private static int[] findTwoSum_Sorting(int[] nums, int target) {
+        Arrays.sort(nums);
+        int left = 0;
+        int right = nums.length - 1;
+        while(left < right) {
+            if(nums[left] + nums[right] == target) {
+                return new int[] {nums[left], nums[right]};
+            } else if (nums[left] + nums[right] < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return new int[] {};
+    }
+
+
+
+}
